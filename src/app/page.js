0c9dc32190af678a9b6c85a0d03ae5f186b037ee
ramdas <ramdassinghmt30@gmail.com>
2025-02-28 23:10:24 +0530

    initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,132 @@
-import Image from "next/image";
+"use client"
+import Head from 'next/head';
+import Link from 'next/link';
+import { Link as ScrollLink } from 'react-scroll';
+import { Typewriter } from 'react-simple-typewriter';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <>
+      <Head>
+        <title>Portfolio | MERN Developer</title>
+        <meta name="description" content="MERN Stack Developer Portfolio" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      
+      <header className="bg-black text-white p-5 fixed top-0 left-0 w-full z-50 shadow-md border-b-2 border-yellow-300"> 
+        <div className="container mx-auto flex justify-between items-center">
+          <h1 className="text-2xl font-bold text-yellow-500">Ramdas Singh</h1>
+          <nav>
+            <ul className="flex space-x-8 text-white">
+            <li><ScrollLink to="hero" smooth={true} duration={500}>Home</ScrollLink></li>
+            <li><ScrollLink to="about" smooth={true} duration={500}>About</ScrollLink></li>
+            <li><ScrollLink to="skills" smooth={true} duration={500}>Skills</ScrollLink></li>
+            <li><ScrollLink to="projects" smooth={true} duration={500}>Projects</ScrollLink></li>
+              <li><Link href="https://www.linkedin.com/in/ramdas-singh/">Linkedin</Link></li>
+              <li><Link href="https://github.com/Ramdas-developer">GitHub</Link></li>
+            </ul>
+          </nav>
         </div>
+      </header>
+       
+      <main className="container bg-black text-white mx-auto py-10 pt-10">
+        {/*hero section */}
+        <section id="hero" className="flex items-center justify-between ms-10 py-10 ">
+          <div className='text-left w-1/2'>
+          <h2 className="text-5xl font-extrabold">Hi, I am </h2>
+          <h1 className="text-6xl font-extrabold text-yellow-400">Ramdas Singh,</h1>
+          <h2 className="mt-3 text-3xl font-bold">
+             I'm a <span className="text-purple-600">
+             <Typewriter
+                words={['MERN Stack Developer', 'React Developer', 'Node.js Developer', 'Full Stack Developer']}
+                loop={0} // Infinite loop
+                cursor
+                cursorStyle="|"
+                typeSpeed={70}
+                deleteSpeed={70}
+                delaySpeed={2000}
+             />
+                 </span>
+          </h2>
+          
+          <h2 className="text-2xl font-bold">Building scalable and efficient web applications using <br/> ReactJs, NextJs, NodeJs, Express and MongoDb.</h2>
+          </div>
+
+          <div className='w-1/2 flex justify-center mt-10'>
+            <img src='/computer1.jpg' alt="profile" className='w-50 h-50 shadow-lg' />
+          </div>
+        </section>
+
+        {/*About*/}
+        <section id="about" className="text-left ms-10 py-20 mt-10">
+          <h1 className="text-6xl text-center font-bold mb-8">About Me</h1>
+          <h2 className="text-4xl font-bold">
+             Hey, I'm <span className="text-yellow-400">Ramdas Singh</span><br/>
+             <span className="text-purple-600"> MERN Stack Developer</span><br/> 
+             who thrives on building **high-performance,<br/> scalable, and user-centric applications.**
+          </h2>
+  
+          <p className="mt-5 text-xl leading-relaxed">
+          I specialize in crafting **blazing-fast** web applications with **React, Next.js, Node.js, Express, and MongoDB.**  
+          My mission? To **solve real-world problems** through **clean code, intuitive UI, and robust backend architecture.**  
+          I don’t just build websites—I create **experiences that drive impact** 🚀.
+          </p>
+
+          <p className="mt-5 text-xl leading-relaxed">
+          I thrive in fast-paced environments, love **debugging the impossible,** and constantly push myself to **learn, adapt, and innovate.**  
+          Whether it’s optimizing APIs for speed, architecting scalable databases, or designing pixel-perfect frontends,  
+          I bring a **problem-solving mindset** and a **get-it-done attitude.**
+          </p>
+
+          <p className="mt-5 text-xl font-bold text-yellow-300">
+          Let’s connect and build something extraordinary together! 💡
+          </p>
+
+          <div className='mt-8'>
+           <a href='/Ramdas Singh Resume.pdf' download className='bg-yellow-500 text-black px-6 py-3 text-lg font-semibold rounded-lg shadow-lg hover:bg-yellow-400 transition duration-300'>
+           📄 Download CV
+           </a>
+          </div>
+        </section>
+        
+        {/*Skills*/}
+        <section id="skills" className="py-10">
+          <h3 className="text-3xl text-center font-bold mb-6">Skills</h3>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            <div className="p-4 m-3 border rounded-lg shadow">
+              <h4 className="text-xl font-semibold">Frontend</h4>
+              <p className="mt-2">Description of the project.</p>
+              <Link href="/projects/project1" className="text-blue-500">View Details</Link>
+            </div>
+            <div className="p-4 border rounded-lg shadow">
+              <h4 className="text-xl font-semibold">Backend</h4>
+              <p className="mt-2">Description of the project.</p>
+              <Link href="/projects/project2" className="text-blue-500">View Details</Link>
+            </div>
+          </div>
+        </section>
+
+        {/*Projects*/}
+        <section id="projects" className="py-10">
+          <h3 className="text-3xl text-center font-bold mb-6">Projects</h3>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            <div className="p-4 m-3 border rounded-lg shadow">
+              <h4 className="text-xl font-semibold">Project 1</h4>
+              <p className="mt-2">Description of the project.</p>
+              <Link href="/projects/project1" className="text-blue-500">View Details</Link>
+            </div>
+            <div className="p-4 border rounded-lg shadow">
+              <h4 className="text-xl font-semibold">Project 2</h4>
+              <p className="mt-2">Description of the project.</p>
+              <Link href="/projects/project2" className="text-blue-500">View Details</Link>
+            </div>
+          </div>
+        </section>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+
+      <footer className="bg-gray-900 text-white text-center p-4">
+        <p>&copy; 2025 Your Name. All rights reserved.</p>
       </footer>
-    </div>
+    </>
   );
 }
